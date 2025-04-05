06fc924091d7e1d85a05f09a0f49a281139528ed
Mathias Poimer <poimerm1@gmail.com>
2025-04-05 11:06:01 +0200

    iOS setup

---
+++ performance-ios/performance-ios/performance_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  performance_iosApp.swift
+//  performance-ios
+//
+//  Created by Mathias Poimer on 05.04.25.
+//
+
+import SwiftUI
+
+@main
+struct performance_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
